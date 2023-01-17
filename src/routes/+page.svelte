f6514d0eb7f7159b05e3301115864685bbb96825
Dana Ng <ngdana1@gmail.com>
2023-01-16 21:03:21 -0800

    Setup cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,38 @@
 <script>
-    // import { base } from "$app/paths";
-    import "../app.css";
-  </script>
-  <div class="max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-    <div class="md:flex">
-      <div class="p-8 m-20">
-        <div class="uppercase tracking-wide text-sm text-teal-500 font-semibold">Easy way to practice a language</div>
-        <h2 class="block mt-1 text-lg leading-tight font-medium">Simple Language Cards</h2>
-        <p class="mt-2 text-slate-400">Trying to find a simple method to study a language? Use our simple language stacks to build up chunks of languages in a pile, shake them up, and start practicing.</p>
-      </div>
-    </div>
-  </div>
+	// import { base } from "$app/paths";
+	import '../app.css';
+</script>
+
+<div class="max-w-md mx-auto md:max-w-2xl">
+	<div class="md:flex">
+		<div class="p-8 m-20">
+			<div class="uppercase tracking-wide text-sm text-teal-500 font-semibold">
+				Easy way to practice a language
+			</div>
+			<h2 class="block mt-1 text-lg leading-tight font-medium">Simple Language Cards</h2>
+			<p class="mt-2 text-slate-400">
+				Trying to find a simple method to study a language? Use our simple language stacks to build
+				up chunks of languages in a pile, shake them up, and start practicing.
+			</p>
+		</div>
+	</div>
+	<div class="max-w-md mx-auto">
+		<div class="flex gap-1 flex-col sm:flex-row items-center content-center">
+			<div class="p-8 bg-teal-400 text-black rounded-md w-1/3 drop-shadow-md hover:drop-shadow-xl ">
+				<p>10</p>
+				<h2>Korean</h2>
+				<p>Food</p>
+			</div>
+      <div class="p-8 bg-teal-400 text-black rounded-md w-1/3 drop-shadow-md hover:drop-shadow-xl ">
+				<p>5</p>
+				<h2>Turkish</h2>
+				<p>Office</p>
+			</div>
+      <div class="p-8 bg-teal-400 text-black rounded-md w-1/3 drop-shadow-md hover:drop-shadow-xl ">
+				<p>6</p>
+				<h2>Hindi</h2>
+				<p>Family</p>
+			</div>
+		</div>
+	</div>
+</div>
