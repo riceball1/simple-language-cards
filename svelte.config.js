519604f81da86d5f4d7db51b4a77875250a3211e
Dana Ng <ngdana1@gmail.com>
2023-01-16 19:17:13 -0800

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 // import { vitePreprocess } from '@sveltejs/kit/vite';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
