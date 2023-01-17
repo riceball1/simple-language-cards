32d4e61861a73c439a7ff495332f3f0cd34457d1
Dana Ng <ngdana1@gmail.com>
2023-01-16 20:36:02 -0800

    style main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script>
-    import { base } from "$app/paths";
+    // import { base } from "$app/paths";
     import "../app.css";
   </script>
-  
-  <h1>Simple Language Cards</h1>
-  <a href="{base}/about">About</a>
+  <div class="max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    <div class="md:flex">
+      <div class="p-8 m-20">
+        <div class="uppercase tracking-wide text-sm text-teal-500 font-semibold">Easy way to practice a language</div>
+        <h2 class="block mt-1 text-lg leading-tight font-medium">Simple Language Cards</h2>
+        <p class="mt-2 text-slate-400">Trying to find a simple method to study a language? Use our simple language stacks to build up chunks of languages in a pile, shake them up, and start practicing.</p>
+      </div>
+    </div>
+  </div>
